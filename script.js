6d806da6d614cee10c85e394d9881317d7df3dc3
gillie1022 <dave.gillie@icloud.com>
2020-05-05 17:53:08 -0400

    emails are on the page, need to figure out how to change string to titlecase

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 // set card target
-let board = document.querySelector(".surface");
+let board = document.querySelector(".board");
 
-let curtis = customers[0]
-console.log(curtis)
-// created card div
-    let card = document.createElement("div")
-    card.classList.add("card")
-    board.appendChild(card)
+for (let customer of customers) {
+  // created card div
+  let card = document.createElement("div");
+  card.classList.add("card");
+  board.appendChild(card);
+  // create pic div
+  // add pic to div
+  // add pic div to card
+  let picDiv = document.createElement("div");
+  picDiv.classList.add("picDiv");
+  picDiv.innerHTML = `<img src=${customer.picture.large}>`;
+  card.appendChild(picDiv);
+  // create name div
+  // create h2 tag for name
+  // add name div to card
+  let nameDiv = document.createElement("div");
+  nameDiv.classList.add("nameDiv");
+  nameDiv.innerHTML = `<h2>${
+    customer.name.first.charAt(0).toUpperCase() +
+    customer.name.first.slice(1) +
+    " " +
+    customer.name.last.charAt(0).toUpperCase() +
+    customer.name.last.slice(1)
+  }</h2>`;
+  card.appendChild(nameDiv);
+// create email div
+// add p tag to div
+// add email div to card
+  let emailDiv = document.createElement("div");
+  emailDiv.classList.add("emailDiv");
+  emailDiv.innerHTML = `<p>${customer.email}</p>`;
+  card.appendChild(emailDiv);
 
 
-    let picDiv = document.createElement("div");
-    picDiv.classList.add("picDiv")
-    picDiv.innerHTML = `<img src=${curtis.picture.large}>`
-
-    card.appendChild(picDiv)
 
 
+}
+function getName() {
+  return;
+}
 
-function createImageElement(url){
-    return `<img src=${url}>`
+function createImageElement(url) {
+  return `<img src=${url}>`;
 }
 // create pic div
 // let pic = document.createElement
